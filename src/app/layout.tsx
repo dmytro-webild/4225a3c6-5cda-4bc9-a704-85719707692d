9d7669a35f909df48f2b9e45f9fcc1c1f553b026
dk <kudin.dima@gmail.com>
2026-05-18 15:39:04 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Open_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Professional Dental Clinic | Expert Oral Health Care',
+  description: 'Expert dental care services including restorations, implants, and routine checkups. Book your visit at our clinic today for a healthy smile.',
+  openGraph: {
+    "title": "DentalClinics - Professional Oral Health Care",
+    "description": "Providing top-tier dental treatments and expert patient care.",
+    "siteName": "DentalClinics",
+    "type": "website"
+  },
+};
+
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${openSans.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
